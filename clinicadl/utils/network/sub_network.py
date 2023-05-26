f118d83b3008d5a995490fe794fb98fb815d13b9
sophieloiz <sophie.loizillon@gmail.com>
2023-05-26 17:53:20 +0200

    Fix DANN with 2 outputs

--- clinicadl/utils/network/sub_network.py
+++ clinicadl/utils/network/sub_network.py
@@ -852,7 +852,7 @@ class CNN_DANN2ouputs(Network):
             else:
                 if flag_t1 == True:
                     t1_tensor = images[i]
-                    t1_label = labels[i].cpu().tolist()
+                    t1_label.append(labels[i])
                     flag_t1 = False
 
                 else:
