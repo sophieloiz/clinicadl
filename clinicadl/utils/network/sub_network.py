7177773be47e05237921e10f66151ead6f3c0cf6
sophieloiz <sophie.loizillon@gmail.com>
2023-05-26 17:09:59 +0200

    Fix DANN with 2 outputs

--- clinicadl/utils/network/sub_network.py
+++ clinicadl/utils/network/sub_network.py
@@ -825,8 +825,9 @@ class CNN_DANN2ouputs(Network):
         )
 
         # Create binary flags for the domain values
+        # Create binary flags for the domain values
         source_domain_flag = domain == "t1"  # 1 if domain is "t1", 0 otherwise
-        target_domain_flag = domain == "flair"  # 1 if domain is "flair", 0 otherwise
+        target_domain_flag = domain == "flair"
 
         logger.info(f"Images shape {images.size()}")
         logger.info(f"Images shape {source_domain_flag}")
