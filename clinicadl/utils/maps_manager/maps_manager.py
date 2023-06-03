f40af46bca062e27b7ab737ff37f9b188173c2af
sophieloiz <sophie.loizillon@gmail.com>
2023-06-03 13:09:57 +0200

    DANN2 : pretraining

--- clinicadl/utils/maps_manager/maps_manager.py
+++ clinicadl/utils/maps_manager/maps_manager.py
@@ -1634,7 +1634,7 @@ class MapsManager:
                     / len(combined_data_loader)
                 )
                 alpha = 2.0 / (1.0 + np.exp(-10 * p)) - 1
-                # alpha = 0.2
+                alpha = 0
                 logger.info(
                     f"Iteration {i} out of {len(combined_data_loader)} with alpha = {alpha}"
                 )
