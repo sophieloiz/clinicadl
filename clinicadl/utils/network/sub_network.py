ad9d37cea54332d44fa3962d680aa4dd228a33ca
sophieloiz <sophie.loizillon@gmail.com>
2023-06-06 10:00:03 +0200

    DANN2 : finetuning optimizer alpha 0.1

--- clinicadl/utils/network/sub_network.py
+++ clinicadl/utils/network/sub_network.py
@@ -1221,7 +1221,7 @@ class CNN_DANN2ouputs(Network):
             self.device
         )
 
-        train_output_class, _, _ = self.forward(images, alpha)
+        _, train_output_class, _ = self.forward(images, alpha)
 
         if use_labels:
             loss = criterion(train_output_class, labels)
