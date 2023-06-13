91a45f549f398fbf7e23c2551d9b1a4aa2b9d57f
sophieloiz <sophie.loizillon@gmail.com>
2023-06-13 15:43:05 +0200

    Pretrain MME baseline

--- clinicadl/utils/caps_dataset/data.py
+++ clinicadl/utils/caps_dataset/data.py
@@ -153,7 +153,6 @@ class CapsDataset(Dataset):
         # Try to find .nii.gz file
         try:
             file_type = self.preprocessing_dict["file_type"]
-            print(file_type)
             results = clinica_file_reader(
                 [participant], [session], self.caps_dict[cohort], file_type
             )
