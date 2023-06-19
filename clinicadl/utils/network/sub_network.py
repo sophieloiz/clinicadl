4e7fc468dcee7a93ea39ba08d6efd85085fb5b3b
sophieloiz <sophie.loizillon@gmail.com>
2023-06-19 10:10:30 +0200

    train our method

--- clinicadl/utils/network/sub_network.py
+++ clinicadl/utils/network/sub_network.py
@@ -1279,7 +1279,7 @@ class CNN_DANN2ouputs(Network):
             self.device
         )
 
-        _, train_output_class, _ = self.forward(images, alpha)
+        train_output_class, _ , _= self.forward(images, alpha)
 
         if use_labels:
             loss = criterion(train_output_class, labels)
