a4577643ca88b466c4d9600db8d8e5e4e0295e23
sophieloiz <sophie.loizillon@gmail.com>
2023-06-26 10:32:34 +0200

    Finetunning for R1

--- clinicadl/utils/maps_manager/maps_manager.py
+++ clinicadl/utils/maps_manager/maps_manager.py
@@ -135,9 +135,9 @@ class MapsManager:
         if self.multi_network:
             self._train_multi(split_list, resume=False)
 
-        elif self.ssda_network:
-            self._train_ssda(split_list, resume=False)
-            # self.train_ssda_baseline(split_list, resume=False)
+        # elif self.ssda_network:
+        #     self._train_ssda(split_list, resume=False)
+        # self.train_ssda_baseline(split_list, resume=False)
 
         else:
             self._train_single(split_list, resume=False)
@@ -5692,17 +5692,18 @@ class MapsManager:
             model.transfer_weights(transfer_state["model"], transfer_class)
 
             # sophie
-            # list_name = [name for (name, _) in model.named_parameters()]
-            # list_param = [param for (_, param) in model.named_parameters()]
+            print("Finetunning")
+            list_name = [name for (name, _) in model.named_parameters()]
+            list_param = [param for (_, param) in model.named_parameters()]
 
-            # for param, _ in zip(list_param, list_name):
-            #     param.requires_grad = False
+            for param, _ in zip(list_param, list_name):
+                param.requires_grad = False
 
-            # for i in range(3 * 2):  # Freeze of the last FC layers
-            #     param = list_param[len(list_param) - i - 1]
-            #     name = list_name[len(list_name) - i - 1]
-            #     param.requires_grad = True
-            #     logger.info(f"Layer {name} freezed {param.requires_grad}")
+            for i in range(3 * 2):  # Freeze of the last FC layers
+                param = list_param[len(list_param) - i - 1]
+                name = list_name[len(list_name) - i - 1]
+                param.requires_grad = True
+                logger.info(f"Layer {name} freezed {param.requires_grad}")
         return model, current_epoch
 
     def _init_optimizer(self, model, split=None, resume=False):
