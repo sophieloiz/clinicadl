b1bd7827cdcdfad15d1788012a8e50ce5d955b43
sophieloiz <sophie.loizillon@gmail.com>
2023-07-05 20:27:38 +0200

    Add MT Networks

--- clinicadl/utils/task_manager/task_manager.py
+++ clinicadl/utils/task_manager/task_manager.py
@@ -200,7 +200,7 @@ class TaskManager:
         with torch.no_grad():
             for i, data in enumerate(dataloader):
                 print("Remove alpha from task manager if no ssda training")
-                outputs, loss_dict = model.compute_outputs_and_loss(
+                outputs, _,loss_dict = model.compute_outputs_and_loss(
                     data, criterion, use_labels=use_labels
                 )  # , alpha=0 features
 
