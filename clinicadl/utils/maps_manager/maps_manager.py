6949d5832c8e1c7c115bc06a45c1b6d352f684db
sophieloiz <sophie.loizillon@gmail.com>
2023-11-07 18:07:18 +0100

    Add hyper-parameters optimization

--- clinicadl/utils/maps_manager/maps_manager.py
+++ clinicadl/utils/maps_manager/maps_manager.py
@@ -1441,7 +1441,7 @@ class MapsManager:
                 alpha = 2.0 / (1.0 + np.exp(-10 * p)) - 1
 
 
-                optimizer = model.lr_scheduler(lr=self.lr, optimizer=optimizer, p=p)
+                optimizer = model.lr_scheduler(lr=0.01, optimizer=optimizer, p=p) # TO MODIFY ;  lr=self.learning_rate
                 optimizer.zero_grad()
 
                 _, _, loss_dict = model.compute_outputs_and_loss(
