e8fdf462d35343357ced61f77474dd17b6398621
sophieloiz <sophie.loizillon@gmail.com>
2024-10-09 09:54:55 +0200

    Inference

--- clinicadl/utils/maps_manager/maps_manager.py
+++ clinicadl/utils/maps_manager/maps_manager.py
@@ -3201,7 +3201,7 @@ class MapsManager:
                 self.maps_path
                 / f"{self.split_name}-{split}"
                 / f"best-{selection_metric}"
-                / "model_29.pth.tar" # TO CHANGE
+                / "model_27.pth.tar" # TO CHANGE
             )
 
         logger.info(
