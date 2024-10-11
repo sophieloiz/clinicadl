69e2a9114d8cde6027e423641a7b2651b876fa7a
sophieloiz <sophie.loizillon@gmail.com>
2024-10-11 15:35:08 +0200

    Modify model name

--- clinicadl/utils/maps_manager/maps_manager.py
+++ clinicadl/utils/maps_manager/maps_manager.py
@@ -3311,7 +3311,7 @@ class MapsManager:
                 self.maps_path
                 / f"{self.split_name}-{split}"
                 / f"best-{selection_metric}"
-                / "model_77.pth.tar" # TO CHANGE
+                / "model_26.pth.tar" # TO CHANGE
             )
 
         logger.info(
