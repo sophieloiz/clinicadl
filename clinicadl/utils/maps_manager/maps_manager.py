fe3d75c7a822ccb831cf214da6578bb1c86f48b0
sophieloiz <sophie.loizillon@gmail.com>
2024-10-25 09:33:59 +0200

    alpha fixed 0.8

--- clinicadl/utils/maps_manager/maps_manager.py
+++ clinicadl/utils/maps_manager/maps_manager.py
@@ -2023,7 +2023,8 @@ class MapsManager:
                 print(f"Batch {i}, Epoch {epoch}, p: {p}")
                 alpha =  2.0 / (1.0 + np.exp(-10 * p))  - 1
 
-                #alpha = 3
+                alpha = 0.8
+                print(f"Fixed alpha to 0.8 ")
 
                 # Classification loss
                 _, _, loss_dict = model.compute_outputs_and_loss_task(
