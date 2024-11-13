edabe8fed60d3b57f21f3cbe29cadf70d486cefd
sophieloiz <sophie.loizillon@gmail.com>
2024-11-13 16:22:48 +0100

    update test

--- clinicadl/utils/maps_manager/maps_manager.py
+++ clinicadl/utils/maps_manager/maps_manager.py
@@ -2403,7 +2403,11 @@ class MapsManager:
             )
             #model = DDP(model)
 
-            prediction_df, metrics = self.task_manager.test_da_debug(
+            # prediction_df, metrics = self.task_manager.test_da_debug(
+            #     model, dataloader, criterion, use_labels=use_labels, # amp=amp
+            # )
+
+            prediction_df, metrics = self.task_manager.test(
                 model, dataloader, criterion, use_labels=use_labels, # amp=amp
             )
             if use_labels:
